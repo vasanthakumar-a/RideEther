c722f9fd4f2e108e5f4618e02598f810c58cbf6e
Vasanthakumar A <vasanthakumara117@gmail.com>
2021-07-11 19:41:59 +0530

    init

--- rideether/models.py
+++ rideether/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    username = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    password1 = models.CharField(max_length=100)
+    password2 = models.CharField(max_length=100) 
--- rideether/views.py
+++ rideether/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render, redirect
+from django.contrib.auth.models import User, auth
 # Create your views here.
 
 def map(request):
     return render(request, 'map.html')
 
 def register(request):
-    return render(request, 'register.html')
+
+    if request.method == 'POST':
+        first_name=request.POST.get["first_name"]
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name, phone_number=phone_number)
+        user.save()
+        print("user created")
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
 
 def login(request):
     return render(request, 'login.html')
