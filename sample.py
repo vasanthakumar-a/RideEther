47ed48587152b31f7140a3e29243e8ff0b80ff2f
Vasanthakumar A <vasanthakumara117@gmail.com>
2021-07-21 22:38:49 +0530

    init

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import gmplot
 
-lat = [10.969644, 10.969644,10.969644]
-lng = [77.008981, 77.008981,77.008981]
+lat = [10.969644, 10.969644,10.90020]
+lng = [77.008981, 77.008981,77.08772]
 gmapOne = gmplot.GoogleMapPlotter(10.969644, 77.008981,15)
 gmapOne.scatter(lat,lng,'red',size=50,marker=False)
 gmapOne.plot(lat,lng,'blue',edge_width=2.5)
