c84819d47f1a3b6ce0b79f7c8b8249c4835a505f
vasanth117 <vasanthakumara117@gmail.com>
2021-09-27 17:59:21 +0530

    init

--- rideether/models.py
+++ rideether/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from toolz.itertoolz import last
 
 class driverDB(models.Model):
     driver_name = models.CharField(max_length=100)
@@ -7,3 +8,15 @@ class driverDB(models.Model):
     username = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     driver_address = models.CharField(max_length=100)
+
+class waitingDB(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+
+class acceptDB(models.Model):
+    driver_address = models.CharField(max_length=50)
+    user_address = models.CharField(max_length=50)
+    accept = models.CharField(max_length=50)
